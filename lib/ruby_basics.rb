71ab41fccc4bdb576977571227e1292cced76d5c
andrel6392 <andrel6392@hstat.org>
2017-10-12 20:08:43 +0000

    pass all tests

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,24 @@
-def division
-    
+def division(num_one, num_two)
+    num_one/num_two
 end
 
-def argue
-  
+def argue(word)
+    word
 end
 
-def greeting
-
+def greeting(greeting, name)
+    greeting + name
 end
 
 def return_a_value
+    "Nice"
     
 end
 
-def pizza_party
-    
+def pizza_party(topping="cheese")
+    topping
 end
 
-def whisper
-    
+def whisper(phrase)
+    phrase.downcase
 end
